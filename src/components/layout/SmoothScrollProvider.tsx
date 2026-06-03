a6f54799fb472a7830f5772564fd0915d72c5e03
Faid Arya Pamungkas <faid.arya.ft24@mail.umy.ac.id>
2026-06-03 07:24:02 +0700

    SmoothScrollProvider

---
+++ src/components/layout/SmoothScrollProvider.tsx
@@ -0,0 +1,40 @@
+'use client'
+
+import { useEffect } from 'react'
+import Lenis from 'lenis'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import { gsap } from 'gsap'
+
+gsap.registerPlugin(ScrollTrigger)
+
+export function SmoothScrollProvider({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      orientation: 'vertical',
+      gestureOrientation: 'vertical',
+      smoothWheel: true,
+      wheelMultiplier: 1,
+      touchMultiplier: 2,
+    })
+
+    // Tell ScrollTrigger to update whenever Lenis scrolls
+    lenis.on('scroll', ScrollTrigger.update)
+
+    // Lenis uses its own RAF loop
+    let rafId: number
+    function raf(time: number) {
+      lenis.raf(time)
+      rafId = requestAnimationFrame(raf)
+    }
+    rafId = requestAnimationFrame(raf)
+
+    return () => {
+      cancelAnimationFrame(rafId)
+      lenis.destroy()
+    }
+  }, [])
+
+  return <>{children}</>
+}
